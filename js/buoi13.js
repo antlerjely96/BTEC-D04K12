b2c722e213fb7e413b1cfbd88065557132bc8359
antlerjely96 <antlerjely96@gmail.com>
2022-02-21 09:56:05 +0700

    add buoi 14

---
+++ js/buoi13.js
@@ -0,0 +1,5 @@
+function demo(){
+    document.getElementById('the_p').innerHTML = "Đây không phải thẻ p";
+    document.getElementById('the_p').title = "thẻ p";
+    document.getElementById('the_p').style.color = "red";
+}
